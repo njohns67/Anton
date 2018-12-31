aff4a2a5498ad2938b6607b69867c324a14060e4
njohns67 <nathan.johnston359@gmail.com>
2018-12-30 23:21:35 -0600

    Reorganized

---
+++ Resources/timer.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int convert(int n, char c){
+    if(c == 'h')
+        n *= 3600;
+    else if(c == 'm')
+        n *= 60;
+    return n;
+}
+
+int main(int argc, char **argv){
+    int num1 = convert(atoi(argv[1]), *argv[2]);
+    int num2;
+    if(argc > 3){
+        num2 = convert(atoi(argv[3]), *argv[4]);
+        sleep(num1 + num2);
+        system("mpg123 Responses/TimerFinish.mp3");
+    }
+    else{
+        sleep(num1);
+        system("mpg123 Responses/TimerFinish.mp3");
+    }
+}
